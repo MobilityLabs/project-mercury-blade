6767c360a122ee29f578b3e1f65d29d7bb5b384a
Nephi Wright <nephiw@gmail.com>
2016-02-09 17:55:58 -0700

    Add some styling to the app - add test DOM

--- client/app/main.jsx
+++ client/app/main.jsx
@@ -2,7 +2,8 @@
 
 const React = require('react'),
       ReactDOM = require('react-dom'),
-      CommentsList = require('./commentsList.jsx');
+      CommentsList = require('./commentsList.jsx'),
+      css = require('!style!css!less!../less/styles.less');
 
 const comments = window.comments;
 
